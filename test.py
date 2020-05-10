c8b61ae6da0d4d33a04d2db34a98e907261c03fa
Chris Arridge <48034138+chrisarridge@users.noreply.github.com>
2020-05-10 19:39:40 +0100

    Update test.py

--- test.py
+++ test.py
@@ -6,13 +6,21 @@ import paths
 
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
 
-t = np.linspace(0,5,100)
-xc = (0 + t*2)*np.cos(t*4)
-yc = (0 + t*2)*np.sin(t*4)
-beziers = paths.fit_cubic_bezier(xc, yc, 0.25)
-
-print(len(beziers))
-
+# Construct points along Archimedian spiral.
+t = np.linspace(0,30,100)
+r = 0 + 0.1*t
+xc = r*np.cos(t)
+yc = r*np.sin(t)
 plt.plot(xc, yc, 'o')
-[a.plot() for a in beziers]
+
+# Fit bezier curves with a maximum error of 0.25.
+beziers = paths.fit_cubic_bezier(xc, yc, 0.25)
+print('Accuracy of 0.25 requires {} bezier patches'.format(len(beziers)))
+[b.plot(color='r') for b in beziers]
+
+# Fit bezier curves with a maximum error of 0.05.
+beziers = paths.fit_cubic_bezier(xc, yc, 0.05)
+print('Accuracy of 0.05 requires {} bezier patches'.format(len(beziers)))
+[b.plot(color='b') for b in beziers]
+
 plt.show()
