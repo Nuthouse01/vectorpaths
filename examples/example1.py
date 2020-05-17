fce750251da388a8f040dc92cb3ab058b679ea4a
Chris Arridge <48034138+chrisarridge@users.noreply.github.com>
2020-05-17 19:35:33 +0100

    Reworked test as example 1 and added control point plotting.

--- examples/example1.py
+++ examples/example1.py
@@ -11,8 +11,9 @@ t = np.linspace(0,30,100)
 r = 0 + 0.1*t
 xc = r*np.cos(t)
 yc = r*np.sin(t)
-plt.plot(xc, yc, 'o')
 
+plt.subplot(1,2,1)
+plt.plot(xc, yc, 'o')
 # Fit bezier curves with a maximum error of 0.25.
 beziers = vectorpaths.fit_cubic_bezier(xc, yc, 0.25)
 print('Accuracy of 0.25 requires {} bezier patches'.format(len(beziers)))
@@ -22,5 +23,15 @@ print('Accuracy of 0.25 requires {} bezier patches'.format(len(beziers)))
 beziers = vectorpaths.fit_cubic_bezier(xc, yc, 0.05)
 print('Accuracy of 0.05 requires {} bezier patches'.format(len(beziers)))
 [b.plot(color='b') for b in beziers]
+plt.xlim([-4,4])
+plt.ylim([-4,4])
+plt.gca().set_aspect('equal')
 
+plt.subplot(1,2,2)
+plt.plot(xc, yc, 'o')
+[b.plot(color='b') for b in beziers]
+[b.plotcontrol() for b in beziers]
+plt.xlim([-4,4])
+plt.ylim([-4,4])
+plt.gca().set_aspect('equal')
 plt.show()
